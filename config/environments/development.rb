f69a8367ab978601f0f7ecf92464cd8fe479bf67
sivakrishna <siva.k@newtechfusion.com>
2014-01-06 11:28:10 +0530

    add1 application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,14 +35,15 @@ Demo::Application.configure do
    config.assets.debug = true
  # config.action_mailer.delivery_method = :test
 
- #  config.action_mailer.delivery_method = :smtp
- #    config.action_mailer.smtp_settings = {
- #    :address => "smtp.gmail.com" ,
- #    :port => 587,
- #    :domain => "domain.of.sender.net" ,:authentication => "plain" ,
- #    :user_name => "sandeep" ,
- #    :password => "12345678" ,
- #    :enable_starttls_auto => true
- # }
-
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :user_name => "schourey1988@gmail.com",
+    :password => "sandeep1988",
+    :enable_starttls_auto => true 
+ }
+ 
 end
