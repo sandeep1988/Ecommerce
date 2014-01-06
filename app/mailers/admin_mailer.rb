f91d8683fbb470de95cd894a6c78edb3449dfed7
sivakrishna <siva.k@newtechfusion.com>
2014-01-06 15:47:17 +0530

    add

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,7 @@
 class AdminMailer < ActionMailer::Base
-  default from: "schourey1988@email.com"
+  default from: "schourey1988@gmail.com"
 
   def user_added
-	mail(:to => "schourey1988@email.com", :subject => "New order added")
+	mail(:to => "schourey1988@gmail.com", :subject => "New order added")
  end
-
 end
