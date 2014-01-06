c36e49edeb95e3a0df3833505eed492e2fb78423
sivakrishna <siva.k@newtechfusion.com>
2014-01-06 11:54:44 +0530

    add3 application.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Demo::Application.initialize!
-# Demo::Application.configure do 
-#  config.action_mailer.delivery_method = :smtp
-#  config.action_mailer.smtp_settings = {
-#  	:address => "smtp.gmail.com",
-#  	:port => 587,
-#     :domain => "gmail.com",
-#     :authentication => "plain",
-#     :user_name => "schourey1988@gmail.com",
-#     :password => "sandeep1988",
-#     :enable_starttls_auto => true	
-#  }
-# end
+Demo::Application.configure do 
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+ 	:address => "smtp.gmail.com",
+ 	:port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :user_name => "schourey1988@gmail.com",
+    :password => "sandeep1988",
+    :enable_starttls_auto => true	
+ }
+end
 
