ac02ebcfb2f37d1888119a0e76e575342d9ff20b
sivakrishna <siva.k@newtechfusion.com>
2014-01-06 11:34:23 +0530

    add2 application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,4 +63,16 @@ Demo::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #  config.action_mailer.delivery_method = :smtp
+   #  config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :authentication => "plain",
+      :user_name => "schourey1988@gmail.com",
+      :password => "sandeep1988",
+      :enable_starttls_auto => true 
+ }
 end
