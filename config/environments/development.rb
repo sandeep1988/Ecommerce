1c973b7f03d3a19d29804bca55b0d2bef7d7c60e
sivakrishna <siva.k@newtechfusion.com>
2014-01-06 11:45:26 +0530

    add3 production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,14 +34,14 @@ Demo::Application.configure do
   # Expands the lines which load the assets
    config.assets.debug = true
  # config.action_mailer.delivery_method = :test
- #  config.action_mailer.delivery_method = :smtp
- #    config.action_mailer.smtp_settings = {
- #    :address => "smtp.gmail.com" ,
- #    :port => 587,
- #    :domain => "domain.of.sender.net" ,:authentication => "plain" ,
- #    :user_name => "sandeep" ,
- #    :password => "12345678" ,
- #    :enable_starttls_auto => true
- # }
-
+   config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com" ,
+    :port => 587,
+    :domain => "domain.of.sender.net" ,:authentication => "plain" ,
+    :user_name => "sandeep.chourey@newtechfusion.com" ,
+    :password => ENV['sandeep1988'],
+    :enable_starttls_auto => true
+ }
+config.action_mailer.raise_delivery_errors = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,13 @@ Demo::Application.configure do
 
   #  config.action_mailer.delivery_method = :smtp
    #  config.action_mailer.delivery_method = :smtp
- config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      :authentication => "plain",
-      :user_name => "schourey1988@gmail.com",
-      :password => "sandeep1988",
-      :enable_starttls_auto => true 
- }
+ # config.action_mailer.smtp_settings = {
+ #      :address => "smtp.gmail.com",
+ #      :port => 587,
+ #      :domain => "gmail.com",
+ #      :authentication => "plain",
+ #      :user_name => "schourey1988@gmail.com",
+ #      :password => "sandeep1988",
+ #      :enable_starttls_auto => true 
+ # }
 end
