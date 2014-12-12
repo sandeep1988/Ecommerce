de195628f9fec6126e8b8fe47605096479354585
hirerorninjas <info@rorninjas.com>
2014-12-12 19:20:38 +0530

    worked on paypal

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -15,8 +15,29 @@ class Cart < ActiveRecord::Base
     def total_price
 	   line_items.to_a.sum { |item| item.total_price }
 	end
+
 	def total_items
 		line_items.sum(:quantity)
-end
+    end
+
+
+def paypal_url(return_url)
+    values = {
+    :business => 'schourey1988-facilitator@gmail.com',
+    :cmd => '_cart',
+    :upload => 1,
+    :return => return_url,
+    :invoice => id
+    }
+    line_items.each_with_index do |item, index|
+      values.merge!({
+        "amount_#{index + 1}" => item.total_price,
+        "item_name_#{index + 1}" => item.product.title,
+        "item_number_#{index + 1}" => item.product.id,
+        "quantity_#{index + 1}" => item.quantity
+      })
+    end
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+  end
 
 end
