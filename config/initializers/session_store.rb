08c3936dfdf4c866ab3b2950939f31e563b8ab6d
hirerorninjas <info@rorninjas.com>
2014-12-19 11:56:05 +0530

    add ssl

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,5 @@ Demo::Application.config.session_store :cookie_store, key: '_demo_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Demo::Application.config.session_store :active_record_store
+Demo::Application.config.session_store :active_record_store
+#Security::Application.config.session_store :active_record_store
