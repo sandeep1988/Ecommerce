17b6d675cd5eb75ce126210b0e535eafeb1013be
hirerorninjas <info@rorninjas.com>
2014-12-30 15:29:59 +0530

    add app/views/layouts/application.html.erb

---
+++ app/assets/javascripts/parts/ScatterSeries.js
@@ -0,0 +1,35 @@
+/**
+ * Set the default options for scatter
+ */
+defaultPlotOptions.scatter = merge(defaultSeriesOptions, {
+	lineWidth: 0,
+	marker: {
+		enabled: true // Overrides auto-enabling in line series (#3647)
+	},
+	tooltip: {
+		headerFormat: '<span style="color:{series.color}">\u25CF</span> <span style="font-size: 10px;"> {series.name}</span><br/>',
+		pointFormat: 'x: <b>{point.x}</b><br/>y: <b>{point.y}</b><br/>'
+	},
+	stickyTracking: false
+});
+
+/**
+ * The scatter series class
+ */
+var ScatterSeries = extendClass(Series, {
+	type: 'scatter',
+	sorted: false,
+	requireSorting: false,
+	noSharedTooltip: true,
+	trackerGroups: ['group', 'markerGroup', 'dataLabelsGroup'],
+	takeOrdinalPosition: false, // #2342
+	singularTooltips: true,
+	drawGraph: function () {
+		if (this.options.lineWidth) {
+			Series.prototype.drawGraph.call(this);
+		}
+	}
+});
+
+seriesTypes.scatter = ScatterSeries;
+
